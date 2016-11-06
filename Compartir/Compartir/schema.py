359c32d87c6f9ba5f455b5e51a8f42dfc74e4164
Abdulwahab Abdulmalik <abdulmalik.abdulwahab@andela.com>
2016-11-06 11:23:36 +0100

    [Feature] add comment to the Compartir schema app

--- Compartir/Compartir/schema.py
+++ Compartir/Compartir/schema.py
@@ -1,4 +1,6 @@
-import graphene
+# using the django-graph-auth package https://github.com/morgante/django-graph-auth
+
+from graphene import Schema
 from graphene import relay, ObjectType
 
 import graph_auth.schema
@@ -11,4 +13,4 @@ class Query(graph_auth.schema.Query, ObjectType):
 class Mutation(graph_auth.schema.Mutation, ObjectType):
     pass
 
-schema = graphene.Schema(query=Query, mutation=Mutation)
+schema = Schema(query=Query, mutation=Mutation)
