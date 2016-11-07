9c3352f3b508a0cc89d794e01a9668957e1b64c8
Abdulwahab Abdulmalik <abdulmalik.abdulwahab@andela.com>
2016-11-07 21:44:40 +0100

    [Feature] add link model

--- Compartir/links/models.py
+++ Compartir/links/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class LinkModel(models.Model):
+
+    url = models.URLField(max_length=300)
+    title = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    # consider the django tag model to add tags
