6a84f3508b0749530919fda56bc4fb6e344a9a3a
Abdulwahab Abdulmalik <abdulmalik.abdulwahab@andela.com>
2016-11-08 16:12:15 +0100

    [Fix] add date created/modified to link model

--- Compartir/links/models.py
+++ Compartir/links/models.py
@@ -8,6 +8,8 @@ class LinkModel(models.Model):
     url = models.URLField(max_length=300)
     title = models.CharField(max_length=100, blank=True)
     description = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey('auth.User', related_name='links')
     # consider the django tag model to add tags
 
