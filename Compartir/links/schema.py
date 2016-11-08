1010d3ff8e417ecb003a1e9f64d74042ce42d370
Abdulwahab Abdulmalik <abdulmalik.abdulwahab@andela.com>
2016-11-08 11:46:47 +0100

    [Fix] authenticate user with jwt

--- Compartir/links/schema.py
+++ Compartir/links/schema.py
@@ -1,8 +1,27 @@
+from django.contrib.auth.middleware import get_user
+
 from graphene import AbstractType, Field, Node
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.types import DjangoObjectType
 
+from rest_framework.request import Request
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
 from links.models import LinkModel
+
+
+def get_user_jwt(request):
+    ''' Get the user class.
+    return the default user object from django if user is authenticated through
+    session or basic. If not authenticate the user through
+    JSONWebTokenAuthentication
+    '''
+    user = get_user(request)
+    if not user.is_authenticated():
+        user_jwt = JSONWebTokenAuthentication().authenticate(Request(request))
+        if user_jwt is not None:
+            return user_jwt[0]
+    return user
 
 
 class LinkNode(DjangoObjectType):
@@ -22,7 +41,8 @@ class Query(AbstractType):
 
     def resolve_all_links(self, args, context, info):
 
-        if not context.user.is_authenticated():
+        # a user object from get_user_jwt
+        user = get_user_jwt(context)
+        if not user.is_authenticated():
             return LinkModel.objects.none()
-        else:
-            return LinkModel.objects.filter(owner=context.user)
+        return LinkModel.objects.filter(owner=user)
